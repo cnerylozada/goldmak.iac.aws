5c62c9903262e9908bd5d16c739bd720096b94a2
cnerylozada <cnerylozada@cnerylozadas-MacBook-Pro.local>
2024-06-29 16:37:03 -0500

    feat: s3 arn and s3 name were exported as outputs

--- serverless.ts
+++ serverless.ts
@@ -3,36 +3,40 @@ import type { AWS } from "@serverless/typescript";
 const serverlessConfiguration: AWS = {
   service: "goldmak-iac-aws",
   frameworkVersion: "3",
-  plugins: ["serverless-esbuild"],
   provider: {
     name: "aws",
     runtime: "nodejs18.x",
     region: "us-east-2",
-    stage: "${top:stage, 'dev'}",
-    apiGateway: {
-      minimumCompressionSize: 1024,
-      shouldStartNameWithService: true,
-    },
+    stage: "${opt:stage, 'dev'}",
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
       NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
     },
   },
+  resources: {
+    Resources: {
+      goldmakS3Bucket: {
+        Type: "AWS::S3::Bucket",
+        Properties: {
+          BucketName: "goldmak.s3.bucket",
+          PublicAccessBlockConfiguration: {
+            BlockPublicPolicy: true,
+          },
+        },
+      },
+    },
+    Outputs: {
+      goldmakS3BucketName: {
+        Value: { Ref: "goldmakS3Bucket" },
+      },
+      goldmakS3BucketArn: {
+        Value: { "Fn::GetAtt": ["goldmakS3Bucket", "Arn"] },
+      },
+    },
+  },
   // import the function via paths
   functions: {},
   package: { individually: true },
-  custom: {
-    esbuild: {
-      bundle: true,
-      minify: false,
-      sourcemap: true,
-      exclude: ["aws-sdk"],
-      target: "node14",
-      define: { "require.resolve": undefined },
-      platform: "node",
-      concurrency: 10,
-    },
-  },
 };
 
 module.exports = serverlessConfiguration;
